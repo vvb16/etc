fb75d1ead00bcd3ee8932a2364b86b53341c4201
Vitaly Belekhov <rmrf17@gmail.com>
2016-07-15 18:32:38 +0600

    Update for XCode 7

--- PJSIP/build-PJSIP-iOS.sh
+++ PJSIP/build-PJSIP-iOS.sh
@@ -16,19 +16,19 @@ export ARCH='-arch armv7'
 make realclean
 ./configure-iphone --with-ssl="${OPENSSL_DIR}"
 make realclean depend all
-find ./ -name '*.a' | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/armv7/
+find ./ -name '*.a' -type f | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/armv7/
 
 export ARCH='-arch armv7s'
 make realclean
 ./configure-iphone --with-ssl="${OPENSSL_DIR}"
 make realclean depend all
-find ./ -name '*.a' | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/armv7s/
+find ./ -name '*.a' -type f | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/armv7s/
 
 export ARCH='-arch arm64'
 make realclean
 ./configure-iphone --with-ssl="${OPENSSL_DIR}"
 make realclean depend all
-find ./ -name '*.a' | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/arm64/
+find ./ -name '*.a' -type f | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/arm64/
 
 export DEVPATH="${XC_BASE}/Platforms/iPhoneSimulator.platform/Developer"
 
@@ -36,24 +36,24 @@ export ARCH='-arch i386'
 make realclean
 ./configure-iphone --with-ssl="${OPENSSL_DIR}"
 make realclean depend all
-find ./ -name '*.a' | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/i386/
+find ./ -name '*.a' -type f | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/i386/
 
 export ARCH='-arch x86_64'
 make realclean
 ./configure-iphone --with-ssl="${OPENSSL_DIR}"
 make realclean depend all
-find ./ -name '*.a' | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/x86_64/
+find ./ -name '*.a' -type f | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/x86_64/
 
 cd ./lib/arm64
-ls -1| sed -e 's/^\(\(.*\)-arm-apple-darwin9\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-arm64-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../armv7
-ls -1| sed -e 's/^\(\(.*\)-arm-apple-darwin9\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-armv7-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../armv7s
-ls -1| sed -e 's/^\(\(.*\)-arm-apple-darwin9\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-armv7s-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../i386
-ls -1| sed -e 's/^\(\(.*\)-arm-apple-darwin9\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-i386-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../x86_64
-ls -1| sed -e 's/^\(\(.*\)-arm-apple-darwin9\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-x86_64-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../
 ls -1 arm64/|xargs -n1 -R -1 -I ^ echo x86_64/^ i386/^ arm64/^ armv7/^ armv7s/^ -create -output ^ |xargs -L1 xcrun lipo
 
