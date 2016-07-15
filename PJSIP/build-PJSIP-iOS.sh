5d612c138412ec7ef09b3ed36671307f6e874a78
Vitaly Belekhov <rmrf17@gmail.com>
2016-07-15 19:08:37 +0600

    Update for XCode 7

--- PJSIP/build-PJSIP-iOS.sh
+++ PJSIP/build-PJSIP-iOS.sh
@@ -45,15 +45,15 @@ make realclean depend all
 find ./ -name '*.a' -type f | grep -v '^\.\/\/lib' | xargs -J ^ mv ^ ./lib/x86_64/
 
 cd ./lib/arm64
-ls -1| sed -e 's/^\(\(.*\)-arm64-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-[^-]*-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../armv7
-ls -1| sed -e 's/^\(\(.*\)-armv7-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-[^-]*-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../armv7s
-ls -1| sed -e 's/^\(\(.*\)-armv7s-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-[^-]*-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../i386
-ls -1| sed -e 's/^\(\(.*\)-i386-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-[^-]*-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../x86_64
-ls -1| sed -e 's/^\(\(.*\)-x86_64-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
+ls -1| sed -e 's/^\(\(.*\)-[^-]*-apple-darwin_ios\.a\)/\1 \2.a/' | xargs -n2 mv
 cd ../
 ls -1 arm64/|xargs -n1 -R -1 -I ^ echo x86_64/^ i386/^ arm64/^ armv7/^ armv7s/^ -create -output ^ |xargs -L1 xcrun lipo
 
